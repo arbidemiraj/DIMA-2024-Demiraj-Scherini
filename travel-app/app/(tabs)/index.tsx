3b873350269dfe5f1ac5b0b467ab7d137adf8573
Scherini Samuele <samuele.scherini@mail.polimi.it>
2024-03-17 14:58:55 +0100

    query for trips

--- travel-app/app/(tabs)/index.tsx
+++ travel-app/app/(tabs)/index.tsx
@@ -2,18 +2,25 @@ import { StyleSheet } from 'react-native';
 import { Text, View } from '@/components/Themed';
 import { supabase } from '@/lib/supabase';
 import { useEffect } from 'react';
+import { QueryResult, QueryData, QueryError } from '@supabase/supabase-js'
+
 export default function TabOneScreen() {
 
   // Test for supabase query -> WORKING CORRECTLY
-  /*   useEffect(() => {
-      getTrips();
-    }, []);
-  
-    const getTrips = async () => {
-      const { data, error } = await supabase.from('trip').select('*');
-      console.log(data);
-    }
-   */
+  useEffect(() => {
+    getTrips();
+  }, []);
+
+  const getTrips = async () => {
+    const { data: trips, error } = await supabase
+      .from('trip')
+      .select(`*, trip_category(*, category(*)), profile_trip(*, profile(username))`)
+      .eq('profile_trip.role', 'author').order('id');
+
+    if (error) throw error;
+    console.dir(trips);
+  }
+
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Explore</Text>

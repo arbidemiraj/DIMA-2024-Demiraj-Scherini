e60bd1614d6363f641952aa08643855ed7ba662c
arbidemiraj <arbi.demiraj@mail.polimi.it>
2024-03-26 15:01:52 +0100

    Google places input style

--- travel-app/components/GooglePlacesInput.tsx
+++ travel-app/components/GooglePlacesInput.tsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import { GooglePlacesAutocomplete } from 'react-native-google-places-autocomplete';
-import { SafeAreaView } from 'react-native';
+import { Platform, SafeAreaView } from 'react-native';
+import FontAwesome from '@expo/vector-icons/FontAwesome';
+import { useColorScheme } from 'react-native';
+import Colors from '@/constants/Colors';
 
 const GooglePlacesInput = () => {
   const apiKey = process.env.EXPO_PUBLIC_PLACES_API_KEY;
@@ -8,35 +11,84 @@ const GooglePlacesInput = () => {
   return (
     <SafeAreaView>
         <GooglePlacesAutocomplete
-        placeholder="Search"
+        placeholder="Discover wonderful experencies..."
         query={{key: apiKey}}
         fetchDetails={true}
         onPress={(data, details = null) => console.log(data, details)}
         onFail={error => console.log(error)}
         onNotFound={() => console.log('no results')}
+        textInputProps={{
+          placeholderTextColor: useColorScheme() === 'light' ? Colors.light.text : Colors.dark.text,
+        }}
         styles={{
           container: {
             flex: 0,
+            marginTop: 50,
+            paddingHorizontal: 20,
           },
           description: {
-            color: '#000',
+            color: useColorScheme() === 'light' ? Colors.light.text : Colors.dark.text,
             fontSize: 16,
+          },
+          textInputContainer: {
+            backgroundColor: useColorScheme() === 'light' ? Colors.light.background : Colors.dark.separator,
+            alignItems: 'center',
+            marginBottom: 10,
+            borderRadius: 30,
+
+            ...Platform.select({
+              ios: {
+                shadowColor: "#000",
+                shadowOffset: {
+                  width: 0,
+                  height: 5,
+                },
+                shadowOpacity: 0.34,
+                shadowRadius: 6.27,
+              },
+              android: {               
+                elevation: 10,
+              },
+            }),
           },
           textInput: {
-            height: 38,
-            color: '#5d5d5d',
-            fontSize: 16,
-            paddingVertical: 5,
-            paddingHorizontal: 10,
-            borderRadius: 5,
+            backgroundColor: useColorScheme() === 'light' ? Colors.light.background : Colors.dark.separator,
+            color: useColorScheme() === 'light' ? Colors.light.text : Colors.dark.text,
+            flex: 1,
+            height: 40,
+            paddingLeft: 5,
+            marginTop: 2,
           },
           predefinedPlacesDescription: {
-            color: '#3caf50',
+            color: useColorScheme() === 'light' ? Colors.light.text : Colors.dark.text,
+          },
+          row: {
+            backgroundColor: useColorScheme() === 'light' ? Colors.light.background : Colors.dark.background, // Dropdown menu color
+          },
+          poweredContainer: {
+            backgroundColor: useColorScheme() === 'light' ? Colors.light.background : Colors.dark.background, // Background color of 'powered by Google' row
           },
         }}
+        renderLeftButton={() => (
+          <FontAwesome
+            name='search'
+            size={22}
+            color={useColorScheme() === 'light' ? Colors.light.text : Colors.dark.text}
+            style={{ marginRight: 10,
+            marginLeft: 15 }} 
+          />
+        )}
+
+        renderRightButton={() => (
+          <FontAwesome
+            name='filter'
+            size={22}
+            color={useColorScheme() === 'light' ? Colors.light.text : Colors.dark.text}
+            style={{marginRight: 15}} 
+          />
+        )}
         />
     </SafeAreaView>
-        
   );
 };
 

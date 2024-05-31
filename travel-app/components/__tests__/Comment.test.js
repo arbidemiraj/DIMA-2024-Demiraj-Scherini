78111f2b308312e046b2c88d3a33c72a131fc7e3
Scherini Samuele <samuele.scherini@mail.polimi.it>
2024-05-31 17:20:34 +0200

    comment tested fix

--- travel-app/components/__tests__/Comment.test.js
+++ travel-app/components/__tests__/Comment.test.js
@@ -11,20 +11,20 @@ describe('Comment component test', () => {
 
     const { getByText } = render(<Comment comment={comment} />);
 
-    // Check if user name is rendered correctly
-    expect(getByText('Test User')).toBeTruthy();
-
-    // Check if comment content is rendered correctly
-    expect(getByText('This is a test comment')).toBeTruthy();
-
-    // Check if the shape of the user name is bold
     const userName = getByText('Test User');
 
+    // Check if user name is rendered correctly
+    expect(userName).toBeTruthy();
+
+    // Check if the shape of the user name is bold
     const userNameStyle = userName.props.style.find((styleObj) => styleObj.fontWeight);
     expect(userNameStyle.fontWeight).toEqual('bold');
 
     // Check if the shape of the user name has a margin bottom
     const userNameMarginBottom = userName.props.style.find((styleObj) => styleObj.marginBottom);
     expect(userNameMarginBottom.marginBottom).toEqual(5);
+
+    // Check if comment content is rendered correctly
+    expect(getByText('This is a test comment')).toBeTruthy();
   });
 });
